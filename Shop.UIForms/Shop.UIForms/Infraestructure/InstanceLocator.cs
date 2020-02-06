4cf380c5512f2362d70d1dcd31265ec9c02720e2
Andrew <afraser@lean-tech.io>
2020-02-06 16:37:07 -0500

    Starter Xamarin Code: Add Login Page, LoginViewModel, some guide notes and configure Service Locator Pattern

---
+++ Shop.UIForms/Shop.UIForms/Infraestructure/InstanceLocator.cs
@@ -0,0 +1,18 @@
+﻿using Shop.UIForms.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop.UIForms.Infraestructure
+{
+    public class InstanceLocator
+    {
+        public MainViewModel Main { get; set; }
+
+        // Locator Pattern for mantain only one ManViewModel instance. 
+        public InstanceLocator()
+        {
+            Main = new MainViewModel();
+        }
+    }
+}
