40f1528b097c0302f11b28f1e1ba8057a0d6014f
Andrew <afraser@lean-tech.io>
2020-02-20 09:45:10 -0500

    Add new entities

---
+++ Shop.Web/Data/Entities/Country.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Web.Data.Entities
+{
+    public class Country : IEntity
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50, ErrorMessage = "The field {0} can contain {1} characters")]
+        [Required]
+        public string Name { get; set; }
+    }
+}
